32270fb6cadba167c3b019c70d615df181c1cb00
mirchandanikaran <85918591+mirchandanikaran@users.noreply.github.com>
2021-08-11 02:38:15 +0530

    Add files via upload
    Shell script to display system information in Debian-based linux distributions.

---
+++ sysinfo.sh
@@ -0,0 +1,14 @@
+clear
+echo ""
+linuxlogo
+lsb_release -irdc
+printf "Kernel Version: ";uname -r
+printf "Processor Type: ";uname -m
+printf "\n"
+printf "RAM Stats: "
+printf "\n"
+free -h
+printf "\n"
+printf "RAM Stats (rounded off):"
+printf "\n"
+free -h --si
